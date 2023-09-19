80c586c1e835d03d8b4194e85e81975c84151be2
ahsen <ahsen@DESKTOP-AP5G745>
2023-09-19 16:15:32 +0300

    Add project files.

---
+++ ip/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelip = new System.Windows.Forms.Label();
+            this.labelmac = new System.Windows.Forms.Label();
+            this.labeluser = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelip
+            // 
+            this.labelip.AutoSize = true;
+            this.labelip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelip.Location = new System.Drawing.Point(131, 124);
+            this.labelip.Name = "labelip";
+            this.labelip.Size = new System.Drawing.Size(36, 25);
+            this.labelip.TabIndex = 0;
+            this.labelip.Text = "IP:";
+            // 
+            // labelmac
+            // 
+            this.labelmac.AutoSize = true;
+            this.labelmac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelmac.Location = new System.Drawing.Point(131, 181);
+            this.labelmac.Name = "labelmac";
+            this.labelmac.Size = new System.Drawing.Size(64, 25);
+            this.labelmac.TabIndex = 1;
+            this.labelmac.Text = "MAC:";
+            // 
+            // labeluser
+            // 
+            this.labeluser.AutoSize = true;
+            this.labeluser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labeluser.Location = new System.Drawing.Point(131, 76);
+            this.labeluser.Name = "labeluser";
+            this.labeluser.Size = new System.Drawing.Size(136, 25);
+            this.labeluser.TabIndex = 2;
+            this.labeluser.Text = "Bilgisayar Adı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeluser);
+            this.Controls.Add(this.labelmac);
+            this.Controls.Add(this.labelip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelip;
+        private System.Windows.Forms.Label labelmac;
+        private System.Windows.Forms.Label labeluser;
+    }
+}
+
---
+++ ip/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Management;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+
+namespace ip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string mac = Mac();
+            string bilgisayarAdi = Dns.GetHostName();
+            labeluser.Text = "Bilgisayar Adı: " + bilgisayarAdi;
+            string ipAdresi = Dns.GetHostByName(bilgisayarAdi).AddressList[1].ToString();
+            labelip.Text = "IP Adresi " + ipAdresi;
+            labelmac.Text = mac;
+        }
+
+        static string Mac()
+        {
+            ManagementClass manager = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            foreach (ManagementObject obj in manager.GetInstances())
+            {
+                if ((bool)obj["IPEnabled"])
+                {
+                    return obj["MacAddress"].ToString();
+                }
+            }
+            return String.Empty;
+        }
+        void macOgren()
+        {
+            string mac = Mac();
+           
+                
+                labelmac.Text = mac;
+            
+        }
+
+    }
+}
